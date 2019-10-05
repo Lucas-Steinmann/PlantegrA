579e26eccc2e742c2357d24a9049ff885b19d954
Luqswe <dupps95@web.de>
2019-10-05 14:13:57 +0200

    add axios and api urls

---
+++ plantegraclient/src/plugins/axiosClient.js
@@ -0,0 +1,120 @@
+import axios from "axios";
+
+class AxiosClient {
+  get(url) {
+    var serverResponse = { hasError: false, data: null };
+    axiosInstance()
+      .get(url)
+      .then(response => {
+        if (response.status == StatusOk) {
+          serverResponse.data = response.data;
+        } else {
+          serverResponse.hasError = true;
+          serverResponse.data = response.data.Message;
+        }
+      })
+      .catch(function(error) {
+        console.log(error);
+        serverResponse.hasError = true;
+        serverResponse.data = "Connection Error";
+        return serverResponse;
+      });
+    return serverResponse;
+  }
+  post(url, data) {
+    var serverResponse = { hasError: false, data: null };
+    axiosInstance()
+      .post(url, data)
+      .then(response => {
+        if (response.status == StatusOk) {
+          serverResponse.data = response.data;
+        } else {
+          serverResponse.hasError = true;
+          serverResponse.data = response.data.Message;
+        }
+      })
+      .catch(function(error) {
+        console.log(error);
+        serverResponse.hasError = true;
+        serverResponse.data = "Connection Error";
+        return serverResponse;
+      });
+    return serverResponse;
+  }
+  put(url, data) {
+    var serverResponse = { hasError: false, data: null };
+    axiosInstance()
+      .put(url, data)
+      .then(response => {
+        if (response.status == StatusOk) {
+          serverResponse.data = response.data;
+        } else {
+          serverResponse.hasError = true;
+          serverResponse.data = response.data.Message;
+        }
+      })
+      .catch(function(error) {
+        console.log(error);
+        serverResponse.hasError = true;
+        serverResponse.data = "Connection Error";
+        return serverResponse;
+      });
+    return serverResponse;
+  }
+  delete(url) {
+    var serverResponse = { hasError: false, data: null };
+    axiosInstance()
+      .delete(url)
+      .then(response => {
+        if (response.status == StatusOk) {
+          serverResponse.data = response.data;
+        } else {
+          serverResponse.hasError = true;
+          serverResponse.data = response.data.Message;
+        }
+      })
+      .catch(function(error) {
+        console.log(error);
+        serverResponse.hasError = true;
+        serverResponse.data = "Connection Error";
+        return serverResponse;
+      });
+    return serverResponse;
+  }
+  patch(url, data) {
+    var serverResponse = { hasError: false, data: null };
+    axiosInstance()
+      .patch(url, data)
+      .then(response => {
+        if (response.status == StatusOk) {
+          serverResponse.data = response.data;
+        } else {
+          serverResponse.hasError = true;
+          serverResponse.data = response.data.Message;
+        }
+      })
+      .catch(function(error) {
+        console.log(error);
+        serverResponse.hasError = true;
+        serverResponse.data = "Connection Error";
+        return serverResponse;
+      });
+    return serverResponse;
+  }
+}
+
+const axiosInstance = () => {
+  // Create instance
+  let instance = axios.create();
+
+  instance.defaults.crossdomain = true;
+  instance.defaults.validateStatus = function() {
+    return true;
+  };
+
+  return instance;
+};
+
+const StatusOk = 200;
+
+export const axiosClient = new AxiosClient();
--- plantegraclient/src/store.js
+++ plantegraclient/src/store.js
@@ -4,7 +4,37 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+  state: {
+    apiserver: {
+      url: "http://localhost:8000/api/",
+      endpoints: {
+        addresses: "addresses/",
+        users: "users/",
+        customers: "customers/",
+        appointments: "appointments/",
+        vehicles: "vehicles/",
+        employees: "employees/"
+      }
+    }
+  },
+  getters: {
+    addresses: state => {
+      return state.apiserver.url + state.apiserver.endpoints.addresses;
+    },
+    users: state => {
+      return state.apiserver.url + state.apiserver.endpoints.users;
+    },
+    customers: state => {
+      return state.apiserver.url + state.apiserver.endpoints.customers;
+    },
+    appointments: state => {
+      return state.apiserver.url + state.apiserver.endpoints.appointments;
+    },
+    vehicles: state => {
+      return state.apiserver.url + state.apiserver.endpoints.vehicles;
+    },
+    employees: state => {
+      return state.apiserver.url + state.apiserver.endpoints.employees;
+    }
+  }
 });
