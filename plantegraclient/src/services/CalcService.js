9913bff34d4a211c90c8e5d85aeb38b3dbb540c1
Jonas Fehrenbach <Jonas.Fehrenbach@synolus.com>
2019-10-25 16:36:08 +0200

    changed week calculation and added hardcoded debug week

--- plantegraclient/src/services/CalcService.js
+++ plantegraclient/src/services/CalcService.js
@@ -1,16 +1,17 @@
 class CalcService {
   getMonday(date) {
-    date = new Date(date);
     var day = date.getDay();
     var diff = date.getDate() - day + (day == 0 ? -6 : 1); // adjust when day is sunday
     return new Date(date.setDate(diff));
   }
   getDaysOfCurrentWeek() {
     var ret = [];
-    ret.push(this.getMonday(new Date()));
-    var monday = this.getMonday(new Date());
-    for (let i = 0; i < 5; i++) {
-      ret.push(new Date(monday.setDate(monday.getDate() + 1)));
+    //var monday = this.getMonday(new Date());
+    //Debug week
+    var monday = new Date(Date.parse("21 Oct 2019"));
+    for (let i = 0; i < 6; i++) {
+      var weekday = new Date();
+      ret.push(new Date(weekday.setDate(monday.getDate() + i)));
     }
     return ret;
   }
